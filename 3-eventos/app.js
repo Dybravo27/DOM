2d34f304f75679803706bb8ba3ab55d3e138de9d
Dylan Bravo <dylanslebyng@gmail.com>
2025-04-23 17:25:49 -0500

    pruebas validaciones y eventos en el nombre,apellido,telefono,documento

--- 3-eventos/app.js
+++ 3-eventos/app.js
@@ -5,18 +5,51 @@ const formulario = document.querySelector('form');
 const nombre = document.querySelector('[name = "nombre"]');
 const apellido = document.querySelector('[name = "apellido"]');
 const telefono = document.querySelector('[name = "telefono"]');
+const documento = document.querySelector('[name = "documento"]');
 const usuario = document.querySelector('[name = "usuario"]');
 const contrasena = document.querySelector('[name = "contrasena"]');
 // const btn = document.querySelector('#btn_validar');
 
 // FUNCIONES
 const validar = (event) => {
-  event.preventDefault();
-  if (nombre.value == "") {
-    alert('Llene es nombre');
+  if (nombre.value.trim() == "") {
+    alert('Ingresa tu nombre completo');
     nombre.focus();
+  }
+  if (apellido.value.trim() == "") {
+    alert('Ingresa tu apellido');
+    apellido.focus();
+  }
+  if (telefono.value.trim() == "") {
+    alert('Ingresa tu número de teléfono');
+    telefono.focus();
+  }
+  if (usuario.value.trim() == "") {
+    alert('Ingresa tu nombre de usuario');
+    usuario.focus();
+  }
+  if (contrasena.value.trim() == "") {
+    alert('Ingresa una contraseña segura');
+    contrasena.focus();
+  }
+  event.preventDefault();
+};
+const validacionSoloLetras = (event) => {
+  const RegExp = /[0-9]/;
+  if (RegExp.test(event.key)) {
+    event.preventDefault();
+  }
+}
+const validacionSoloNumeros = (event) => {
+  const RegExp = /^[a-zA-Z]$/;
+  if (RegExp.test(event.key)) {
+    event.preventDefault();
   }
 }
 
 // EVENTOS
 formulario.addEventListener('submit',validar);
+nombre.addEventListener('keydown',validacionSoloLetras);
+apellido.addEventListener('keydown',validacionSoloLetras);
+telefono.addEventListener('keydown',validacionSoloNumeros);
+documento.addEventListener('keydown',validacionSoloNumeros);
